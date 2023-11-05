c05f6fcfaaa11817f21e015d5ef05e15c3ca258a
Dan kleeman <dan.kleeman.git@pm.me>
2023-11-05 15:49:18 -0600

    Updating documentation.

--- app/server.py
+++ app/server.py
@@ -10,8 +10,9 @@ logger = utils.setup_logger()
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--logging-level", dest="logging_level", default="INFO")
+    parser.add_argument("--host_port", dest="host_port", default="5556", type=str)
     args = vars(parser.parse_args())
     logger.setLevel(args.pop("logging_level"))
 
-    bot = LoadBalancerBot(host_port='5556')
+    bot = LoadBalancerBot(**args)
     bot.run(config["auth"]["discord_token"])
